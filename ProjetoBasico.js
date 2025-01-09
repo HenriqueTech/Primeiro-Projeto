a895cad74d41a022a36c975a3d3124913136251e
HenriqueTech <henriquesilva.tech@hotmail.com>
2025-01-09 20:14:42 -0300

    Primeiro Projeto

---
+++ ProjetoBasico.js
@@ -0,0 +1,28 @@
+let nomeHeroi = "Henrique"
+let experiencia = 10200
+
+if(experiencia === 1000)
+{
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Ferro")
+}
+else if (experiencia > 1001 && experiencia < 2000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Bronze")
+}
+else if (experiencia > 2001 && experiencia < 5000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Prata")
+}
+else if (experiencia > 5001 && experiencia < 7000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Ouro")
+}
+else if (experiencia > 7001 && experiencia < 8000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Platina")
+}
+else if (experiencia > 8001 && experiencia < 9000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Ascendente")
+}
+else if (experiencia > 9001 && experiencia < 10000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Imortal")
+}
+else if (experiencia >= 10000){
+    console.log ("O Heroi de nome: " + nomeHeroi,"Está no nivel " + experiencia, "E é Radiante")
+}
